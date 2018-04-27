d894f2ba8797751dc6ed86a57f5283c50ac31fe5
Gian Calgeer <gidoca@gmail.com>
2018-04-27 19:26:53 +0000

    VkFunctionPointer (works now)

--- VulkanWinTut/VkFunctionPointer.h
+++ VulkanWinTut/VkFunctionPointer.h
@@ -1,28 +1,38 @@
-#pragma once
+#ifndef VK_FUNCTION_POINTER_HEADER
+#define VK_FUNCTION_POINTER_HEADER
+
+#include <stdexcept>
 
 #define VK_INSTANCE_FUNC(instance, name) VkInstanceFunctionPointer<PFN_##name> name{(instance), #name};
 
-template <class R, class... A>
-struct return_type<R(*)(A...)>
-{
-	typedef R type;
-};
+template<typename F>
+struct ReturnType { };
 
-template<Function>
+template<typename R, typename... A>
+struct ReturnType<R(A...)> { typedef R Type; };
+
+template<typename R, typename... A>
+struct ReturnType<R(*)(A...)> { typedef R Type; };
+
+template<typename Function>
 class VkInstanceFunctionPointer {
 public:
+	typedef typename ReturnType<Function>::Type RetType;
+
 	VkInstanceFunctionPointer(VkInstance instance, const char* name) {
 		func = (Function)vkGetInstanceProcAddr(instance, name);
 		if (func == nullptr) {
-			throw std::runtime_exception
+			throw std::runtime_error("");
 		}
 	}
 
 	template<typename... A>
-	return_type<Function>::type operator()(A... args) {
-		return func(args);
+	RetType operator()(A... args) {
+		return func(args...);
 	}
 
 private:
 	Function func;
 };
+
+#endif
